cd4cf1f06472d9dfec59a0d125c3ebf8f0398808
Pravalika <jellapravalika655@gmail.com>
2026-09-25 12:02:31 +0530

    Create half_adder_tb.v

---
+++ half_adder/half_adder/half_adder_tb.v
@@ -0,0 +1,40 @@
+`timescale 1ns/1ps
+
+module half_adder_tb;
+
+reg a;
+reg b;
+
+wire sum;
+wire carry;
+
+half_adder dut (
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+
+initial begin
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+initial begin
+    $monitor("Time=%0t | A=%b B=%b | Sum=%b Carry=%b",
+             $time, a, b, sum, carry);
+end
+
+endmodule
